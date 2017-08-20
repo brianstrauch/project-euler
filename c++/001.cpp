48207d8a061eee5ebd78dbb7eb0e13131761a651
Brian Strauch <bstrauch24@gmail.com>
2017-08-20 15:32:02 -0500

    Renamed folder

---
+++ c++/001.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int sum = 0;
+	for(int n = 1; n < 1000; n++) {
+		if(n % 3 == 0 || n % 5 == 0) {
+			sum += n;
+		}
+	}
+	cout << sum << endl;
+}
