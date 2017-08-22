5dca082397e8f1c118c88d6f7922a96ac3a0a430
Brian Strauch <bstrauch24@gmail.com>
2017-08-22 14:04:21 -0500

    Added #1-2

---
+++ java/P002.java
@@ -0,0 +1,16 @@
+public class P002 {
+	public static void main(String[] args) {
+		int sum = 0;
+		int a = 1, b = 2;
+		while(a < 4000000) {
+			if(a % 2 == 0) {
+				sum += a;
+			}			
+
+			int c = a + b;
+			a = b;
+			b = c;
+		}
+		System.out.println(sum);
+	}
+}
