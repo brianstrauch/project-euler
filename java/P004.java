3c8bcf5b84aec6d536c2cb911e5cb674bec58d04
Brian Strauch <bstrauch24@gmail.com>
2017-08-23 17:50:48 -0500

    Added #4

---
+++ java/P004.java
@@ -0,0 +1,23 @@
+public class P004 {
+	private static boolean palindrome(int n) {
+		int forward = n, reverse = 0;
+		while(n > 0) {
+			reverse = 10 * reverse + n % 10;
+			n /= 10;
+		}
+		return forward == reverse;
+	}
+
+	public static void main(String[] args) {
+		int largestPalindrome = 0;
+		for(int a = 100; a < 1000; a++) {
+			for(int b = a; b < 1000; b++) {
+				int product = a * b;
+				if(product > largestPalindrome && palindrome(product)) {
+					largestPalindrome = product;
+				}
+			}
+		}
+		System.out.println(largestPalindrome);
+	}
+}
