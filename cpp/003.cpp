1241598d11356c9bbfe80c8f91e9b708e7e4e638
Brian Strauch <bstrauch24@gmail.com>
2018-08-06 21:24:20 -0500

    Rewrite

--- cpp/003.cpp
+++ cpp/003.cpp
@@ -3,13 +3,10 @@ using namespace std;
 
 int main() {
 	long n = 600851475143;
-
-	long largest_factor = 0;
-	for(long i = 2; i <= n; i++) {
+	for(long i = 2; i < n; i++) {
 		while(n % i == 0) {
-			largest_factor = i;
 			n /= i;
 		}
 	}
-	cout << largest_factor << endl;
+	cout << n << endl;
 }
