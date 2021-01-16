8344c911e7aa6b1976642eb32a6e41c49f19276a
Brian Strauch <bstrauch24@gmail.com>
2021-01-16 17:30:00 -0600

    #10

--- go/010.go
+++ go/010.go
@@ -3,26 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	n := 1000000
+	n := 2000000
 
 	isPrime := make([]bool, n)
 	for i := 0; i < n; i++ {
 		isPrime[i] = true
 	}
 
-	count := 0
-
+	sum := 0
 	for i := 2; i < n; i++ {
 		if isPrime[i] {
 			for j := 2 * i; j < n; j += i {
 				isPrime[j] = false
 			}
-
-			count++
-			if count == 10001 {
-				fmt.Println(i)
-				break
-			}
+			sum += i
 		}
 	}
+	fmt.Println(sum)
 }
