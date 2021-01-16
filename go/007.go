66d3a3d0b198dd73f26eb880090e9d6b2c035cbf
Brian Strauch <bstrauch24@gmail.com>
2021-01-16 16:40:30 -0600

    #7

---
+++ go/007.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 1000000
+
+	isPrime := make([]bool, n)
+	for i := 0; i < n; i++ {
+		isPrime[i] = true
+	}
+
+	isPrime[0] = false
+	isPrime[1] = false
+
+	count := 0
+
+	for i := 2; i < n; i++ {
+		if isPrime[i] {
+			for j := 2 * i; j < n; j += i {
+				isPrime[j] = false
+			}
+
+			count++
+			if count == 10001 {
+				fmt.Println(i)
+				break
+			}
+		}
+	}
+}
