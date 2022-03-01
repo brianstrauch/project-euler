faecae94522ed2efe961ca21c3f057bf732361a0
Brian Strauch <bstrauch@confluent.io>
2022-02-28 20:52:37 -0800

    #2

---
+++ exs/002.exs
@@ -0,0 +1,15 @@
+defmodule Problem002 do
+    def solve(a, _, sum) when a > 4000000 do
+        sum
+    end
+
+    def solve(a, b, sum) do
+        if rem(a, 2) == 0 do
+            solve(b, a + b, sum + a)
+        else
+            solve(b, a + b, sum)
+        end
+    end
+end
+
+IO.puts(Problem002.solve(0, 1, 0))
