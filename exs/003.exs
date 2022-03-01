ea038288ab6cbcb5eece184b0da73c6536814411
Brian Strauch <bstrauch@confluent.io>
2022-02-28 21:53:19 -0800

    #3

---
+++ exs/003.exs
@@ -0,0 +1,15 @@
+defmodule Problem003 do
+    def solve(n, d) when n == d do
+        n
+    end
+
+    def solve(n, d) do
+        if rem(n, d) == 0 do
+            solve(div(n, d), d)
+        else
+            solve(n, d + 1)
+        end
+    end
+end
+
+IO.puts(Problem003.solve(600851475143, 2))
